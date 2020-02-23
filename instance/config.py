74673c9c2889c584ea25dbfe89a61a33a1e1d7ae
Gurudatt Shanbhag <gurushan13@gmail.com>
2020-02-23 19:42:07 +0530

    #db dump added

--- instance/config.py
+++ instance/config.py
@@ -1,11 +1,11 @@
 import os
 
-SECRET = os.environ['SECRET']
+SECRET = os.environ.get('SECRET') or "12345"
 
 # MONGO CONFIG
-MONGO_HOST = os.environ['MONGO_HOST']
-MONGO_PORT = int(os.environ['MONGO_PORT'])
-MONGO_DBNAME = os.environ['MONGO_DBNAME']
+MONGO_HOST = os.environ.get('MONGO_HOST') or 'localhost'
+MONGO_PORT = int(os.environ.get('MONGO_PORT') or 27017)
+MONGO_DBNAME = os.environ.get('MONGO_DBNAME') or 'singapore'
 
 # MONGO COLLECTIONS
 MONGO_COL_HOTELS = 'hotels'
